f42638e4e82aa7434ecba4aab2b4cad7a44746d3
JavaSyntaxError <thiel.erlangen@web.de>
2020-06-29 01:10:36 +0200

    Fixed JSON BUG

--- core/src/main/java/de/lif/schule/core/product/ProductSender.java
+++ core/src/main/java/de/lif/schule/core/product/ProductSender.java
@@ -1,11 +1,14 @@
 package de.lif.schule.core.product;
 
+import org.apache.commons.codec.binary.StringUtils;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.jsoup.internal.StringUtil;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class ProductSender {
 
@@ -28,13 +31,24 @@ public class ProductSender {
     }
 
     private String createProductJson(ProductData productData) {
-        return "{\"marketName\":\"Netto" +
-                "\",\"categoryName\":\"" + productData.getCategory() +
-                "\",\"productName\":\"" + productData.getName() +
-                "\",\"productInfo\":\"" + productData.getDescription() +
-                "\",\"currentPrice\":\"" + productData.getPrice() +
-                "\",\"rabbatPrice\":\"" + "" +
-                "\",\"productGrammage\":\"" + productData.getGrammage() + "\"}";
+        try {
+            String category = new String(productData.getCategory().getBytes(), StandardCharsets.UTF_8);
+            String name = new String(productData.getName().getBytes(), StandardCharsets.UTF_8);
+            String description = new String(productData.getDescription().getBytes(), StandardCharsets.UTF_8);
+            double price = productData.getPrice();
+            int grammage = productData.getGrammage();
+
+            return String.format("{\"marketName\":\"Netto\"," +
+                    "\"categoryName\":\"{0}\"," +
+                    "\"productName\":\"{1}\"," +
+                    "\"productInfo\":\"{2}\"," +
+                    "\"currentPrice\":\"{3}\"," +
+                    "\"rabbatPrice\":\"{4}\"," +
+                    "\"productGrammage\":\"{5}\"}", category, name, description, price, "", grammage);
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
     }
 
     private String replaceBadChars(String s) {
